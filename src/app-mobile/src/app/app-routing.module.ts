9a35c4744eb022b4ed09aa0764ef5f96b8db1c25
Rodrigo Cichetto <rodrigocichetto@gmail.com>
2018-10-24 16:52:40 -0300

    wip login

--- src/app-mobile/src/app/app-routing.module.ts
+++ src/app-mobile/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  // { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  // { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

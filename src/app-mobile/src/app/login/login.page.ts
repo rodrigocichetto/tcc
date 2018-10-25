caf9448d601dec457f0e93711cc5c5e17619f827
Rodrigo Cichetto <rodrigocichetto@gmail.com>
2018-10-25 16:12:01 -0300

    wip login

--- src/app-mobile/src/app/login/login.page.ts
+++ src/app-mobile/src/app/login/login.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
+
+import { UserOptions } from '../interfaces/user-options';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user: UserOptions = {
+    username: '',
+    password: ''
+  };
+
+  constructor(
+    private alertController: AlertController,
+    private translate: TranslateService
+  ) { }
+
+  login(form: NgForm) {
+    if (form.valid) {
+      console.info('valid', this.user);
+    } else {
+      this.presentAlert();
+    }
+  }
+
+  private async presentAlert() {
+    const translated: any = await this.translate.get(['ERROR', 'LOGIN_INVALID', 'OK']);
+    
+    const alert = await this.alertController.create({
+      header: translated.value.ERROR,
+      message: translated.value.LOGIN_INVALID,
+      buttons: [{
+        text: translated.value.OK,
+        cssClass: 'color--dark'
+      }]
+    });
+
+    alert.present();
+  }
 
   ngOnInit() {
   }

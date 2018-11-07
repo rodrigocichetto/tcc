b2e2e342c46d16a8b4f6d502e8a598bf15655916
rodrigocichetto <rodrigocichetto@gmail.com>
2018-11-07 19:03:46 -0200

    fix mongodb docker connection error

--- src/api/config/database.js
+++ src/api/config/database.js
@@ -6,4 +6,17 @@ module.exports = (url) => {
     mongoose.connection.on('connected', () => {
         console.log('Mongoose conectado!');
     });
+
+    // Retry connection
+    const connectWithRetry = () => {
+        console.log('MongoDB connection with retry')
+        return mongoose.connect(url, { useNewUrlParser: true });
+    }
+
+    // Exit application on error
+    mongoose.connection.on('error', err => {
+        console.log(`MongoDB connection error: ${err}`);
+        setTimeout(connectWithRetry, 5000);
+        // process.exit(-1)
+    })
 }
